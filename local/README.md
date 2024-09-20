96817d4b652509e82f687d86d9e0980901b7a74e
Francisco Lopez <flopezlasanta@gmail.com>
2024-09-20 15:04:47 +0200

    added notebooks

---
+++ local/README.md
@@ -0,0 +1,32 @@
+### Step by Step
+
+# Project Overview
+
+This project contains two Jupyter notebooks:
+
+1. **[Order Book](orderbook.ipynb)**: This notebook is used to plot an order book persisted in Parquet format as a test for the `persist_derive` crate.
+2. **[Coinbase RFQs](coinbase.ipynb)**: This notebook plots RFQs from Coinbase.
+
+### Step by Step
+
+```bash
+# Install prerequisites via Homebrew
+brew install pyenv pyenv-virtualenv poetry
+
+# Create and activate the virtual environment
+pyenv install 3.11
+pyenv virtualenv 3.11 <your-env-name>
+pyenv activate <your-env-name>
+
+# Install dependencies
+poetry install
+
+# Use VSCode to select the virtual environment and run the notebook (see below)
+```
+
+### Open the Jupyter Notebook in VSCode
+
+1. Open the project folder in **VSCode**.
+2. Use the command palette (`Ctrl + Shift + P` or `Cmd + Shift + P` on macOS) to select the Python interpreter.
+3. Choose the virtual environment created (`<your-env-name>`).
+4. Open your Jupyter notebook, ensuring the virtual environment is selected as the kernel.
--- record_persist/tests/main.rs
+++ record_persist/tests/main.rs
@@ -5,13 +5,13 @@ mod tests {
     
     use record_persist::{config::PersistConfig, writer::TableWriter};
 
-    use crate::fixture::{get_tmp_folder, Simple};
+    use crate::fixture::{get_tmp_folder, OrderBook};
 
 
     #[test]
     fn test_random_simple_instance() {
-        let instance = Simple::random_instance();
-
+        let instance = OrderBook::random_instance(100, 200, 50.0);
+        print!("{instance}");
         assert!(instance.exchange_id >= 1 && instance.exchange_id < 1000);
         assert!(instance.symbol_id >= 1 && instance.symbol_id < 1000);
         assert!(!instance.buys.is_empty());
@@ -30,12 +30,15 @@ mod tests {
         };
 
         let mut writer = TableWriter::new("simple", &config)?;
-        for _ in 0..5 {
-            let instance = Simple::random_instance();
+
+        let mut instance = OrderBook::random_instance(100, 200, 50.0);
+        writer.begin()?.record(&instance)?.end()?;
+
+        for _ in 0..10 {
+            instance = instance.tick();
             writer.begin()?.record(&instance)?.end()?;
             writer.flush_if_needed()?;
         }
-
         writer.flush()?;
 
         Ok(())
@@ -43,7 +46,7 @@ mod tests {
 }
 
 mod fixture {
-    use std::{collections::HashMap, env};
+    use std::{collections::HashMap, env, fmt, time::{SystemTime, UNIX_EPOCH}};
     use rand::Rng;
 
     use record_persist_derive::Persist;
@@ -56,55 +59,166 @@ mod fixture {
         path_buf.into_os_string().into_string().expect("invalid path")
     }
 
-    #[derive(Debug, Clone, Default, Persist)]
-    pub struct Simple {
-        pub exchange_id: u32,
-        pub symbol_id: u32,
-        pub buys: Vec<f64>,
-        pub sells: Vec<f64>,
-        pub tob: (f64, f64),
-
-        pub properties: HashMap<String, String>,
-
-        pub healthy: bool,
-        #[persist(ignore = false)]
-        pub stale: bool,
-        #[persist(ignore = true)]
-        pub ignore: bool,
-
-        #[persist_timestamp(unit = "ns")]
-        pub internal_ts: u64,
-        #[persist_timestamp(unit = "ns")]
-        pub exchange_ts: Option<u64>,
+    #[derive(Debug, Clone, Persist)]
+    pub struct PriceLevel {
+        pub price: f64,
+        pub quantity: f64,
+    }
+    
+    impl fmt::Display for PriceLevel {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "px: {:.2}, qty: {:.2}", self.price, self.quantity)
+        }
     }
 
-    impl Simple {
-        pub fn random_instance() -> Self {
+    pub type Trade = PriceLevel;
+
+    #[derive(Debug, Clone, Persist)]
+    pub struct OrderBook {
+        pub exchange_id: u32,
+        pub symbol_id: u32,
+        pub buys: Vec<PriceLevel>,
+        pub sells: Vec<PriceLevel>,
+        pub tob: (PriceLevel, PriceLevel),
+        pub trades: Vec<Trade>,
+        pub properties: HashMap<String, String>,
+        pub healthy: bool,
+        pub stale: bool,
+        pub ignore: bool,
+        pub exchange_ts: u64,
+        pub internal_ts: u64,
+    }
+    
+    impl OrderBook {
+        pub fn random_instance(exchange_id: u32, symbol_id: u32, mid_price: f64) -> Self {
             let mut rng = rand::thread_rng();
-            let buys = (0..rng.gen_range(1..5)).map(|_| rng.gen_range(0.0..100.0)).collect();
-            let sells = (0..rng.gen_range(1..5)).map(|_| rng.gen_range(0.0..100.0)).collect();
     
+            let buy_quantities = (0..5)
+                .map(|_| rng.gen_range(1.0..10.0))
+                .collect::<Vec<f64>>();
+            let sell_quantities = (0..5)
+                .map(|_| rng.gen_range(1.0..10.0))
+                .collect::<Vec<f64>>();
+
+            let buys: Vec<PriceLevel> = buy_quantities
+                .iter()
+                .enumerate()
+                .map(|(i, &quantity)| PriceLevel {
+                    price: mid_price - (i + 1) as f64 * rng.gen_range(0.1..1.0),
+                    quantity,
+                })
+                .collect();
+            let sells: Vec<PriceLevel> = sell_quantities
+                .iter()
+                .enumerate()
+                .map(|(i, &quantity)| PriceLevel {
+                    price: mid_price + (i + 1) as f64 * rng.gen_range(0.1..1.0),
+                    quantity,
+                })
+                .collect();
+            let tob = (buys[0].clone(), sells[0].clone());
+    
+            let trades: Vec<Trade> = (0..rng.gen_range(1..=5))
+            .map(|_| Trade {
+                price: rng.gen_range(tob.0.price..=tob.1.price),
+                quantity: rng.gen_range(1.0..10.0),
+            })
+            .collect();
+
             let mut properties = HashMap::new();
-            properties.insert("property1".to_string(), "value1".to_string());
-            properties.insert("property2".to_string(), "value2".to_string());
+            properties.insert("service".to_string(), "test".to_string());
     
-            Simple {
-                exchange_id: rng.gen_range(1..1000),
-                symbol_id: rng.gen_range(1..1000),
+            let exchange_ts = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("time went backwards")
+                .as_nanos() as u64;
+            let internal_ts = exchange_ts + rng.gen_range(1_000..100_000);
+
+            OrderBook {
+                exchange_id,
+                symbol_id,
                 buys,
                 sells,
-                tob: (rng.gen_range(0.0..100.0), rng.gen_range(0.0..100.0)),
+                tob,
+                trades,
                 properties,
                 healthy: rng.gen_bool(0.5),
                 stale: rng.gen_bool(0.5),
                 ignore: rng.gen_bool(0.5),
-                internal_ts: rng.gen_range(1_000_000..1_000_000_000),
-                exchange_ts: if rng.gen_bool(0.5) {
-                    Some(rng.gen_range(1_000_000..1_000_000_000))
-                } else {
-                    None
-                },
+                exchange_ts,
+                internal_ts,
             }
+        }
+
+        pub fn tick(&self) -> Self {
+            let mut rng = rand::thread_rng();
+    
+            let mid_price = ((self.tob.0.price + self.tob.1.price) / 2.0) + rng.gen_range(-0.5..0.5);
+    
+            let buys: Vec<PriceLevel> = self.buys.iter()
+                .enumerate()
+                .map(|(i, pl)| PriceLevel {
+                    price: mid_price - i as f64 * rng.gen_range(0.1..1.0),
+                    quantity: pl.quantity,
+                })
+                .collect();
+            let sells: Vec<PriceLevel> = self.sells.iter()
+                .enumerate()
+                .map(|(i, pl)| PriceLevel {
+                    price: mid_price + i as f64 * rng.gen_range(0.1..1.0),
+                    quantity: pl.quantity,
+                })
+                .collect();
+            let tob = (buys[0].clone(), sells[0].clone());
+
+            let trades: Vec<Trade> = (0..rng.gen_range(1..=5))
+            .map(|_| Trade {
+                price: rng.gen_range(tob.0.price..=tob.1.price),
+                quantity: rng.gen_range(1.0..10.0),
+            })
+            .collect();
+
+            let exchange_ts = self.exchange_ts + rng.gen_range(1_000..10_000);
+            let internal_ts = exchange_ts + rng.gen_range(1_000..100_000);
+
+            OrderBook {
+                exchange_id: self.exchange_id,
+                symbol_id: self.symbol_id,
+                buys,
+                sells,
+                tob,
+                trades,
+                properties: self.properties.clone(),
+                healthy: self.healthy,
+                stale: self.stale,
+                ignore: self.ignore,
+                exchange_ts,
+                internal_ts,
+            }
+        }
+    }
+
+    impl fmt::Display for OrderBook {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(
+                f,
+                "[exchange_id: {}, symbol_id: {}]\n\
+                exchange_ts: {}\n\
+                internal_ts: {}\n\
+                top: ({}, {})\n\
+                buys: [\n{}\n]\n\
+                sells: [\n{}\n]\n\
+                trades: [\n{}\n]",
+                self.exchange_id,
+                self.symbol_id,
+                self.exchange_ts,
+                self.internal_ts,
+                self.tob.0.price,
+                self.tob.1.price,
+                self.buys.iter().map(|b| format!("  {}", b)).collect::<Vec<String>>().join(",\n"),
+                self.sells.iter().map(|s| format!("  {}", s)).collect::<Vec<String>>().join(",\n"),
+                self.trades.iter().map(|s| format!("  {}", s)).collect::<Vec<String>>().join(",\n"),
+            )
         }
     }
 }
